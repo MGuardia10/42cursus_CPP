aabebfcb8d5a4141c0207eaf0ea7244e82fc9a3a
Miguel Guardia <mike.guardia0309@gmail.com>
2024-04-24 10:34:37 +0200

    CPP_01: ex05 done

---
+++ CPP_01/ex05/Harl.cpp
@@ -0,0 +1,42 @@
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug( ) {
+	std::cout << GREEN << "[ DEBUG ]" << RESET << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
+	std::cout << "I really do!" << std::endl << std::endl;
+}
+
+void Harl::info( ) {
+	std::cout << BLUE << "[ INFO ]" << RESET << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
+}
+
+void Harl::warning( ) {
+	std::cout << MAG << "[ WARNING ]" << RESET << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl << std::endl;
+}
+
+void Harl::error( ) {
+	std::cout << RED << "[ ERROR ]" << RESET << std::endl;
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
+}
+
+void Harl::complain( std::string level ) {
+
+	void (Harl::*function[4])(void) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+	std::string stringArr[4] = { "DEBUG", "INFO", "WARNING", "ERROR" };
+
+	for (int i = 0; i < 4; i++) {
+		if (level.compare(stringArr[i]) == 0) {
+			(this->*function[i])();
+			break ;
+		}
+	}
+
+}
---
+++ CPP_01/ex05/main.cpp
@@ -0,0 +1,13 @@
+#include "Harl.hpp"
+
+int main(void) {
+
+	Harl alvaro;
+
+	alvaro.complain( "DEBUG" );
+	alvaro.complain( "INFO" );
+	alvaro.complain( "WARNING" );
+	alvaro.complain( "ERROR" );
+
+	return 0;
+}
