b6f141e94d46706af4746a3463b6dfbef6f38914
Miguel Guardia <mike.guardia0309@gmail.com>
2024-06-10 10:32:24 +0200

    CPP_06: ex01 main changed

--- CPP_06/ex01/main.cpp
+++ CPP_06/ex01/main.cpp
@@ -6,13 +6,13 @@ int main() {
 
     // Convertir puntero a uintptr_t
 	uintptr_t uIntPtr = Serializer::serialize( &a );
-    std::cout << "El valor del puntero convertido a uintptr_t en decimal: " << uIntPtr << std::endl;
-    std::cout << "en hexadecimal: " << std::showbase << std::hex << uIntPtr << std::endl << std::endl;
+    std::cout << "uIntPtr Decimal value (Address of a): " << uIntPtr << std::endl;
+    std::cout << "uIntPtr Hexadecimal value (Address of a): " << std::showbase << std::hex << uIntPtr << std::endl << std::endl;
 
     // Convertir uintptr_t de vuelta a puntero
 	Data *ptrConvertedBack = Serializer::deserialize( uIntPtr );
-    std::cout << "El valor apuntado por el puntero convertido de vuelta: " << ptrConvertedBack->getStr() << std::endl;
-    std::cout << "La direccion del puntero convertido de vuelta: " << ptrConvertedBack << std::endl;
+    std::cout << "ptrConvertedBack string value: " << ptrConvertedBack->getStr() << std::endl;
+    std::cout << "ptrConvertedBack Address: " << ptrConvertedBack << std::endl;
 
     return 0;
 }
