19ac4da8e9e2ee26371ce2082caab53964c1d866
Miguel Guardia <mike.guardia0309@gmail.com>
2024-06-18 14:16:35 +0200

    CPP_09: files created

---
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+class BitcoinExchange {
+	private:
+
+	public:
+
+};
---
+++ CPP_09/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "BitcoinExchange.hpp"
+
+int main( int argc, char **argv ) {
+	if (argc != 2) {
+		std::cerr << "Invalid number of arguments. Try: ./btc <db_file>\n";
+		return 1;
+	}
+
+	
+	
+	return 0;
+}
